c5ab69e0b1b2c20000e25d349226f0de98d81546
DESKTOP-5F7JJF9\Arrow <kaunghtetnaing.lso2015@gmail.com>
2021-10-17 19:10:20 +0630

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class  MyApp extends StatelessWidget {
+  const MyApp ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App 10',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Form")
+        ),
+        body: const Forms(),
+      ),
+    );
+  }
+}
+
+class Forms extends StatefulWidget {
+  const Forms({ Key? key }) : super(key: key);
+
+  @override
+  _FormsState createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+  final keys = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Form(
+      key: keys,
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter your name",
+                  labelText: "Name",
+                  icon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter your Password",
+                  labelText: "Password",
+                  icon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (keys.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              )
+            ],
+        ),
+      )
+      );
+
+  }
+}
